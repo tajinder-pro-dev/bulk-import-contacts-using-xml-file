4496b927e02e7857dddf76b3f608ad74c2b01a60
Tajinder Singh <tajindersingh70@gmail.com>
2025-05-24 12:54:36 +0530

    cache clear

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::get('contacts/import-files', [ContactController::class, 'index']);
+Route::get('/', [ContactController::class, 'index']);
 Route::post('contacts/import-files/xml', [ContactController::class, 'store'])->name('contacts.importfile.xml');
